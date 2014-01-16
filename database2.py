cc0d191e1fde24c04ec9842c895c080415291638
Tyler Hoyt <tyler.hoyt@gmail.com>
2014-01-16 00:02:08 -0800

    working data api

--- database2.py
+++ database2.py
@@ -6,15 +6,16 @@ from flask import g, Flask, render_template, jsonify, request
 DATABASE = 'db/ashrae.db'
 app = Flask(__name__)
 
-def json2csv(json_in):
-    headers = json_in[0].keys()
-    r_csv = ','.join(headers) + '\n'  
-    for r in json_in:
-        r_csv += ','.join([str(v) for v in r.values()]) + '\n'
-    return r_csv 
 
 def query_db(query, args=(), one=False):
-    cur = get_db().execute(query, args)
+    """
+    this is an easy way to get dicts back from the db,
+    though sometimes you have to do it manually like
+    in data()
+    """
+    db = get_db()
+    db.row_factory = make_dicts
+    cur = db.execute(query, args)
     rv = cur.fetchall()
     cur.close()
     return (rv[0] if rv else None) if one else rv
@@ -27,7 +28,6 @@ def get_db():
     db = getattr(g, '_database', None)
     if db is None:
         db = g._database = sqlite3.connect(DATABASE)
-    db.row_factory = make_dicts
     return db
 
 @app.teardown_appcontext
@@ -38,16 +38,7 @@ def close_connection(exception):
 
 @app.route('/buildings', methods = ['GET'])
 def buildings():
-    fields = request.args.get('field')
-    c = get_db().cursor()
-    c.execute('select * from building')
-    d = []
-    while True:
-        _d = c.fetchone()
-        if _d is not None:
-            d.append(c.fetchone())
-        else:
-            break
+    b = query_db('select * from building')
     return render_template('buildings.html', buildings=d)
 
 @app.route('/api/query', methods = ['GET'])
@@ -56,21 +47,26 @@ def query():
     Query API for the raw data
     """
     fields = request.args.get('fields')
-    print fields
     if fields is None:
         return "{}"
     else:
         fields = fields.split(',')
-        print fields
         field_range = range(len(fields))
         select = ['t' + str(i) + '.value' for i in field_range]
         tables = ['data t' + str(i) for i in field_range]
         where = ['t' + str(i) + '.fieldid="' + fields[i] + '" ' for i in field_range]
         where += ['t0.respid=t' + str(i) + '.respid ' for i in range(1, len(fields))]
+        select_str = ', '.join(select)
+        tables_str = ', '.join(tables)
+        where_str = 'and '.join(where)
+ 
+        # not secure
         query = 'select ' + ', '.join(select) + ' from ' + \
                 ', '.join(tables) + ' where ' + 'and '.join(where) + ';'
 
-        res = []
+        c = get_db().cursor()
+        c.execute(query)
+        rv = []
         while True:
             d = c.fetchone()
             if d is not None:
@@ -79,8 +75,8 @@ def query():
                     r[fields[i]] = d[i]
             else:
                 break
-            res.append(r)
-        return json.dumps(res)
+            rv.append(r)
+        return json.dumps(rv)
 
 
 @app.route('/data', methods = ['GET'])
@@ -90,11 +86,8 @@ def data():
 
 @app.route('/api/fields', methods = ['GET'])
 def get_fields():
-    c = get_db().cursor()
-    res = []
-    for row in c.execute('select id from field'):
-        res.append(row)
-    return json.dumps(res)
+    rv = query_db('select * from field')
+    return json.dumps(rv)
 
 @app.route('/')
 def index():
@@ -103,8 +96,6 @@ def index():
 if __name__ == '__main__':
     app.run(debug=True)
 
-# query api
 # data tables
 # render a csv
 # proxies
-# sass
